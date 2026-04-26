c1ac7b8a3142a643bbeec475aaaf0e4b8095f591
Ruslan Nazarov <nazarovruslan16112001@gmail.com>
2026-04-26 20:45:22 +0300

    feat: add global UserRateThrottle (2000/hour) for all authenticated endpoints

--- crm_backend/config/settings/base.py
+++ crm_backend/config/settings/base.py
@@ -207,9 +207,11 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
     ],
     "DEFAULT_THROTTLE_RATES": {
         "anon": "100/hour",
+        "user": "2000/hour",
         "login": "5/m",
         "password_reset": "3/m",
         "user_read": "1000/hour",
